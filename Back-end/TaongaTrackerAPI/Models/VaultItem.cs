13e37c4eaf94ef317015911890f03082370ba625
Oliver Grönkrans <oliver@gronkrans.com>
2025-05-05 11:51:33 +1200

    Fixed nullable properties and added constructor

--- Back-end/TaongaTrackerAPI/Models/VaultItem.cs
+++ Back-end/TaongaTrackerAPI/Models/VaultItem.cs
@@ -4,18 +4,37 @@ namespace TaongaTrackerAPI.Models
     {
         private int VaultItemId;
         private int CurrentOwnerId;
-        private int CreatorId;
-        private List<int> PreviousOwnerIds;
-        private decimal EstimatedValue;
-        private DateTime CreationDate;
-        private DateTime DateAqcuired;
-        private string CreationPlace;
-        private string ItemType;
-        private string PhotoUrl;
-        private string Description;
+        private int? CreatorId;
+        private List<int>? PreviousOwnerIds;
+        private decimal? EstimatedValue;
+        private DateTime? CreationDate;
+        private DateTime? DateAqcuired;
+        private string? CreationPlace;
+        private string? ItemType;
+        private string? PhotoUrl;
+        private string? Description;
         private string Title;
-        private List<string> Materials;
-        private List<string> CraftType;
-        private List<string> SharedWithIds;
+        private List<string>? Materials;
+        private List<string>? CraftType;
+        private List<string>? SharedWithIds;
+
+        public VaultItem(int vaultItemId, int currentOwnerId, int? creatorId = null, List<int>? previousOwnerIds = null, decimal? estimatedValue = null, DateTime? creationDate = null, DateTime? dateAqcuired = null, string? creationPlace = null, string? itemType = null, string? photoUrl = null, string? description = null, string title = null, List<string>? materials = null, List<string>? craftType = null, List<string>? sharedWithIds = null)
+        {
+            VaultItemId = vaultItemId;
+            CurrentOwnerId = currentOwnerId;
+            CreatorId = creatorId;
+            PreviousOwnerIds = previousOwnerIds;
+            EstimatedValue = estimatedValue;
+            CreationDate = creationDate;
+            DateAqcuired = dateAqcuired;
+            CreationPlace = creationPlace;
+            ItemType = itemType;
+            PhotoUrl = photoUrl;
+            Description = description;
+            Title = title;
+            Materials = materials;
+            CraftType = craftType;
+            SharedWithIds = sharedWithIds;
+        }
     }
 }
